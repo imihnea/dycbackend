edc522227bd2e573ec017110cf05d4388e9968c2
Popa Nicolae-Adrian <adrianpopa97@gmail.com>
2018-12-21 15:41:59 +0200

    Sidebar & Footer - Styling

---
+++ dist/js/sidebar.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const currentLocation = window.location.pathname;
+  switch (currentLocation) {
+    case ('/dashboard'): {
+      document.querySelector('#settings').classList.add('active-page');
+      break;
+    }
+    case ('/dashboard/addresses'): {
+      document.querySelector('#addresses').classList.add('active-page');
+      break;
+    }
+    case ('/dashboard/messages'): {
+      document.querySelector('#messages').classList.add('active-page');
+      break;
+    }
+    case ('/dashboard/tokens'): {
+      document.querySelector('#tokens').classList.add('active-page');
+      break;
+    }
+    case ('/dashboard/new'): {
+      document.querySelector('#new').classList.add('active-page');
+      break;
+    }
+    case ('/dashboard/closed'): {
+      document.querySelector('#closed').classList.add('active-page');
+      break;
+    }
+    case ('/dashboard/purchases'): {
+      document.querySelector('#purchases').classList.add('active-page');
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+});
