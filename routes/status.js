d02452e40843a2d28348b6549740e25f609fbec8
Ionita Mihnea <ionitamihneadjgogu@gmail.com>
2019-02-15 13:02:33 +0200

    small changes

--- routes/status.js
+++ routes/status.js
@@ -1,18 +1,14 @@
-const app = new (require('express').Router)();
-const Product = require('../models/product');
+const app = new (require('express').Router)()
 
 app.get('/paybear/status/:order', (req, res) => {
-  var orderId = req.params.order;
-  Product.findByIdAndUpdate(req.params.id, {'orderId': orderId});
-var confirmations = null;
-  
-confirmations = Product.findById(req.params.id, { confirmations: confirmations }); //get from DB, see callback.js
-maxConfirmations = 3; //get from DB, see callback.js
-var resp = {
-  success: confirmations >= maxConfirmations
-};
-if(confirmations !== null)
-  resp.confirmations = confirmations;
-res.json(resp); //return this data to PayBear form
-});
-module.exports = app;
+  // const orderId = req.params.order
+  const confirmations = 0 // get from DB, see callback.php
+  const maxConfirmations = 3 // get from DB, see callback.php
+  const resp = {
+    success: confirmations >= maxConfirmations
+  }
+  if (confirmations !== null) { resp.confirmations = confirmations }
+  res.json(resp) // return this data to PayBear form
+})
+
+module.exports = app
