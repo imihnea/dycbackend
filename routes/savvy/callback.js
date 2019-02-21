b91b21e92fff66572c5afa3fe2cb8689fff3526a
Ionita Mihnea <ionitamihneadjgogu@gmail.com>
2019-02-22 00:02:10 +0200

    New approach to callback

--- routes/savvy/callback.js
+++ routes/savvy/callback.js
@@ -2,53 +2,18 @@ const app = new (require('express').Router)();
 const Checkout = require('../../models/checkout');
 
 app.post('/savvy/callback/:order', (req, res) => {
-  console.log("hit callback");
-  console.log(`Asta e req.body: ${req.body}`);
-  console.log(`Asta e req.params.order: ${req.params.order}`);
-
   var orderId = req.params.order;
+  console.log('order id = ', orderId);
+  console.log('callback body = ', req.body);
+  console.log('---------------');
   var data = req.body;
-  console.log(body);
-  console.log(data);
   var invoice = data.invoice;
+  var amount_payed = data.inTransaction.amount / Math.power(10, data.inTransaction.exp);
+  console.log('amount payed ===', amount_payed);
 
-  var query = { orderId: orderId };
-
-  //save data.confirmations - number of confirmations to DB
-  Checkout.findOneAndUpdate(query, { confirmations: data.confirmations },  function(err) {
-    if(err){
-      res.send(err);
-    }
-    console.log('hit save data.confirmations')
-  });
-
-  if(data.confirmations >= data.maxConfirmations) {
-    var amountPaid = data.inTransaction.amount / Math.pow(10, data.inTransaction.exp);
-    //compare $amountPaid with order total
-    Checkout.findOne(query, 'orderTotal', function(err, orderTotal, next) {
-      if(err) {
-        res.send(err);
-      } else {
-        if(amountPaid !== orderTotal) {
-          res.send(`wrong amount paid`);
-        }
-      }
-    });
-    //compare $invoice with one saved in the database to ensure callback is legitimate
-    Checkout.findOne(query, 'invoice', function(err, invoice, next) {
-      if(err) {
-        res.send(err);
-      } else if(invoice !== data.invoice) {
-        res.send('wrong invoice');
-      }
-    });
-    //mark the order as paid
-    Checkout.findOneAndUpdate(query, { paid: true },  function(err) {
-      if(err){
-        res.send(err);
-      }
-    });
-    res.send(invoice); //stop further callbacks
+  console.log(req.body.confirmations);
+  if (req.body.confirmations >= 3) {
+    res.send(invoice);
   } else {
     res.send('waiting for confirmations');
   }
