66fcc911a50c158e81e4926ee64b5b086af56b9f
Popa Nicolae-Adrian <adrianpopa97@gmail.com>
2019-04-09 20:04:14 +0300

    Responsive dashboard + small fixes

--- dist/js/sidebar.js
+++ dist/js/sidebar.js
@@ -45,4 +45,16 @@ document.addEventListener('DOMContentLoaded', () => {
       break;
     }
   }
+
+  const burgers = Array.prototype.slice.call(document.getElementsByClassName('.navbar-burger .dashboardBurger'), 0);
+  if (burgers.length > 0) {
+    burgers.forEach((el) => {
+      el.addEventListener('click', () => {
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
 });
