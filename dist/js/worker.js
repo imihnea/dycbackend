70d8d3ed0d07e8124e59aeb2b16b8c9ea7eb1312
Popa Nicolae-Adrian <adrianpopa97@gmail.com>
2019-05-30 16:14:31 +0300

    Working notification

--- dist/js/worker.js
+++ dist/js/worker.js
@@ -4,7 +4,7 @@ self.addEventListener("push", e => {
   const data = e.data.json();
   console.log("Push Recieved...");
   self.registration.showNotification(data.title, {
-    body: "Notified by Traversy Media!",
-    icon: "http://image.ibb.co/frYOFd/tmlogo.png"
+    body: "Notified by Deal Your Crypto!",
+    icon: "/dist/img/rollo.png"
   });
 });
