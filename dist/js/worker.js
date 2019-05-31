5d37bac8b35004fce1c08f5fa063a550d790ed75
Popa Nicolae-Adrian <adrianpopa97@gmail.com>
2019-05-31 14:41:46 +0300

    18

--- dist/js/worker.js
+++ dist/js/worker.js
@@ -1,6 +1,7 @@
 console.log("Service Worker Loaded...");
 
 self.addEventListener("push", e => {
+  console.log(e);
   const data = e.data.json();
   console.log("Push Recieved...");
   self.registration.showNotification(data.title, {
