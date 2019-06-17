ae72b7aec4ef0461839b0541d2a8b16691d0b427
Popa Nicolae-Adrian <adrianpopa97@gmail.com>
2019-06-17 15:20:10 +0300

    ES Heroku fix

--- config/elasticsearch.js
+++ config/elasticsearch.js
@@ -3,12 +3,10 @@ const elasticsearch = require('elasticsearch');
 client = new elasticsearch.Client({
     host: 'https://elastic:E37nCmTkobiqfwIAHeGOfrsz@994125d4daf04d4e999fc2c3ccdb5a13.eu-central-1.aws.cloud.es.io:9243',
 });
-// if (process.env.HEROKU == true) {
-// } else {
-//     client = new elasticsearch.Client({
-//         host: 'localhost:9200',
-//     });
-// }
+
+// client = new elasticsearch.Client({
+//     host: 'localhost:9200',
+// });
 
 client.ping({
   requestTimeout: 3000
