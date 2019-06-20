ea64422779f3e14b7dfb1b3a0f136a5a13def28c
Popa Nicolae-Adrian <adrianpopa97@gmail.com>
2019-06-20 19:47:09 +0300

    ES Fix

--- config/elasticsearch.js
+++ config/elasticsearch.js
@@ -35,94 +35,92 @@ client.indices.exists({index: 'products'}, (err, result) => {
                   if (err) {
                     console.log(err);
                   } else {
-                    console.log(result);
+                    console.log(result);// Create mapping
+                    client.indices.getMapping({index: 'products'}, (err, res) => {
+                        if (err) {
+                        console.log(err);
+                        } else {
+                            if (!res.products.mappings) {
+                                client.indices.putMapping(
+                                    {
+                                        index: 'products',
+                                        body: {
+                                            "id": "text",
+                                            "feat_1": {
+                                                "properties": {
+                                                    "status": "boolean",
+                                                    "expiryDate": "date"
+                                                }
+                                            },
+                                            "name": "text",
+                                            "image": "text",
+                                            "author": {
+                                                "properties": {
+                                                    "id": "text",
+                                                    "name": "text",
+                                                    "city": "text",
+                                                    "country": "text",
+                                                    "state": "text",
+                                                    "continent": "text",
+                                                    "accountType": "text"
+                                                }
+                                            },
+                                            "avgRating": "float",
+                                            "btcPrice": "float",
+                                            "condition": "text",
+                                            "category": "text",
+                                            "createdAt": "date",
+                                            "searchableTags": "keyword"
+                                        }
+                                    }, {
+                                }, (err, res) => {
+                                    if (err) {
+                                        console.log(err);
+                                    } else {
+                                        console.log(res);
+                                    }
+                                });
+                            }
+                        }
+                    });
+
+                    // Index available products
+                    Product.find({available: 'True'}, (err ,res) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            res.forEach(product => {
+                                client.index({
+                                    index: 'products',
+                                    type: 'products',
+                                    id: `${product._id}`,
+                                    body: {
+                                        id: product._id,
+                                        feat_1: product.feat_1,
+                                        image: product.images[0].url,
+                                        name: product.name,
+                                        author: product.author,
+                                        avgRating: product.avgRating,
+                                        btcPrice: product.btcPrice,
+                                        condition: product.condition,
+                                        category: product.category,
+                                        createdAt: product.createdAt,
+                                        searchableTags: product.searchableTags
+                                    }
+                                }, function(err, resp, status) {
+                                    if (err) {
+                                        console.log(err);
+                                    }
+                                });
+                            });
+                            console.log('Product indexing process complete');
+                        }
+                    });
                   }
                 });
               }
             });
         }
-    }
-});
-
-// Create mapping
-client.indices.getMapping({index: 'products'}, (err, res) => {
-    if (err) {
-       console.log(err);
-    } else {
-        if (!res.products.mappings) {
-            client.indices.putMapping(
-                {
-                    index: 'products',
-                    body: {
-                        "id": "text",
-                        "feat_1": {
-                            "properties": {
-                                "status": "boolean",
-                                "expiryDate": "date"
-                            }
-                        },
-                        "name": "text",
-                        "image": "text",
-                        "author": {
-                            "properties": {
-                                "id": "text",
-                                "name": "text",
-                                "city": "text",
-                                "country": "text",
-                                "state": "text",
-                                "continent": "text",
-                                "accountType": "text"
-                            }
-                        },
-                        "avgRating": "float",
-                        "btcPrice": "float",
-                        "condition": "text",
-                        "category": "text",
-                        "createdAt": "date",
-                        "searchableTags": "keyword"
-                    }
-                }, {
-            }, (err, res) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(res);
-                }
-            });
-        }
-    }
-});
-
-// Index available products
-Product.find({available: 'True'}, (err ,res) => {
-    if (err) {
-        console.log(err);
-    } else {
-        res.forEach(product => {
-            client.index({
-                index: 'products',
-                type: 'products',
-                id: `${product._id}`,
-                body: {
-                    id: product._id,
-                    feat_1: product.feat_1,
-                    image: product.images[0].url,
-                    name: product.name,
-                    author: product.author,
-                    avgRating: product.avgRating,
-                    btcPrice: product.btcPrice,
-                    condition: product.condition,
-                    category: product.category,
-                    createdAt: product.createdAt,
-                    searchableTags: product.searchableTags
-                }
-            }, function(err, resp, status) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-        });
-        console.log('Product indexing process complete');
     }
 });
 
