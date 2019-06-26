0f4807fc556de2ba4214613dc98c948875d1e7a7
Popa Nicolae-Adrian <adrianpopa97@gmail.com>
2019-06-26 16:45:00 +0300

    Adding tags when pressing enter

--- dist/js/dashboard_new.js
+++ dist/js/dashboard_new.js
@@ -285,6 +285,47 @@ document.addEventListener('DOMContentLoaded', () => {
   let deleteTagItems = [].slice.call(deleteTagBtns);
   const addButton = document.getElementById('addTag');
   let times = tagsControl.children.length;
+  const KEY_ENTER = 13;
+  tagInput.addEventListener('keydown', (e) => {
+    let key = e.charCode || e.keyCode || e.which;
+    if ((key == KEY_ENTER) && (tagInput.value.length > 1) && (times < 10)) {
+      e.preventDefault();
+      times += 1;
+      if (times == 10) {
+        tagInput.placeholder = 'You have reached the tag limit';
+        tagInput.disabled = true;
+        tagInput.classList.add('parsley-error');
+      }
+      // Add the tag to tag input that gets sent
+      tags.value += ' ' + cleanHTML(String(tagInput.value));
+      // Create the tag element
+      tagsControl.innerHTML += `<span class="tag is-link is-medium">${cleanHTML(String(tagInput.value))}<button type="button" class="delete deleteTag is-small"></button></span>`;
+      // Clean the input
+      tagInput.value = '';
+      // Remake the array of delete buttons with the new tag
+      deleteTagBtns = document.querySelectorAll('.deleteTag');
+      deleteTagItems = [].slice.call(deleteTagBtns);
+      // Create the tag deletion event
+      deleteTagItems.forEach((item) => {
+        item.addEventListener('click', () => {
+          const text = item.parentElement.innerText;
+          const regex = new RegExp('\\b' + text + '\\b');
+          // Remove the tag
+          tags.value = tags.value.replace(regex, '');
+          // Remove extra spaces
+          tags.value = tags.value.replace(/\s+/g, ' ').trim();
+          item.parentElement.remove();
+          times -= 1;
+          if (tagInput.classList.contains('parsley-error')) {
+            tagInput.classList.remove('parsley-error');
+            tagInput.disabled = false;
+            tagInput.placeholder = 'Tags';
+          }
+        });
+      });
+    }
+  });
+
   addButton.addEventListener('click', () => {
     if ((tagInput.value.length > 1) && (times < 10)) {
       times += 1;
