6486f440e2fc39e600d35399286da32d71a229d7
Popa Nicolae-Adrian <adrianpopa97@gmail.com>
2019-07-02 14:39:40 +0300

    Truncating

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,17 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const element = document.querySelectorAll('.dealname');
-    const elementItems = [].slice.call(element);
-    let truncated;
-    let i = 0;
-    elementItems.forEach((item) => {
-        truncated = item.textContent;
-        if (truncated.length > 27) {
-            truncated = truncated.substr(0, 27);
-            truncated += '...';
-        }
-        elementItems[i].textContent = truncated;
-        i += 1;
-    });
     
     const maxToShow = 7;
     let showMore = document.querySelector('.showMore');
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const element = document.querySelectorAll('.dealname');
+  const elementItems = [].slice.call(element);
+  let truncated;
+  let i = 0;
+  elementItems.forEach((item) => {
+      truncated = item.textContent;
+      if (truncated.length > 27) {
+          truncated = truncated.substr(0, 27);
+          truncated += '...';
+      }
+      elementItems[i].textContent = truncated;
+      i += 1;
+  });
+
   // Get all dropdown elements
   const dropdown = document.querySelectorAll('.dropdown');
   // Create an array with them
