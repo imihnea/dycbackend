5b31d70a856655e94393fa127113aa163150e2a8
Popa Nicolae-Adrian <adrianpopa97@gmail.com>
2019-07-05 14:04:05 +0300

    Progressive proof

--- controllers/deals.js
+++ controllers/deals.js
@@ -938,10 +938,12 @@ module.exports = {
                 await cloudinary.v2.uploader.upload(req.file.path, 
                 {
                     moderation: "aws_rek:suggestive:ignore",
-                    // transformation: [
+                    transformation: [
                     // {quality: "jpegmini:1", sign_url: true},
                     // {width: "auto", dpr: "auto"}
-                    // ]
+                    {angle: 0},
+                    {flags: 'progressive:semi'}
+                    ]
                 }, (err, result) => {
                     if(err) {
                     errorLogger.error(`Status: ${err.status || 500}\r\nMessage: ${err.message}\r\nURL: ${req.originalUrl}\r\nMethod: ${req.method}\r\nIP: ${req.ip}\r\nUserId: ${req.user._id}\r\nTime: ${moment(Date.now()).format('DD/MM/YYYY HH:mm:ss')}\r\n`);
