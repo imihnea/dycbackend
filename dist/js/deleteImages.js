f497494a25b3c89cd667fdea38b4643ad86d3a2a
Popa Nicolae-Adrian <adrianpopa97@gmail.com>
2019-07-08 15:22:16 +0300

    Removing thumbnails from edit once deleted

--- dist/js/deleteImages.js
+++ dist/js/deleteImages.js
@@ -5,11 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const delLabels = document.querySelectorAll('.file-delete__label');
     const delLabelItems = [].slice.call(delLabels);
     let deleteImages = document.getElementById('deleteImages');
+    let thumbnails = document.querySelectorAll('.thumbnail');
+    let thumbnailItems = [].slice.call(thumbnails);
     delLabelItems.forEach((item, i) => {
         item.addEventListener('click', () => {
             if (images[i]) {
                 deleteImages.value += images[i].public_id + ' ';
                 images[i].public_id = '';
+                thumbnailItems[i].remove();
             }
         });
     });
